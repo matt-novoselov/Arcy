5ce6697a5579b37b77474cfdc9877e6f52347c59
Matt Novoselov <matveynovoselov@gmail.com>
2024-05-03 23:11:36 +0200

    Added Lottie Files animations, restructured projects groups

--- Source/Views/LikeButtonView.swift
+++ Source/Views/LikeButtonView.swift
@@ -12,13 +12,32 @@ struct LikeButtonView: View {
     @Binding var isLiked: Bool
     
     var body: some View {
-        Button(action: {isLiked.toggle()}, label: {
+        Button(action: {
+            withAnimation(.none){
+                isLiked.toggle()
+            }
+        }, label: {
             Image(systemName: isLiked ? "heart.fill" : "heart")
                 .foregroundStyle(isLiked ? .red : .white)
+                .symbolEffect(.bounce, value: isLiked)
         })
+        .padding()
+        .overlay{
+            if isLiked{
+                UILottieView(lottieName: "like_animation", playOnce: true)
+                    .allowsHitTesting(false)
+            }
+        }
     }
 }
 
 #Preview(windowStyle: .automatic) {
-    LikeButtonView(isLiked: .constant(true))
+    struct Preview: View {
+        @State var isLiked: Bool = false
+        var body: some View {
+            LikeButtonView(isLiked: $isLiked)
+        }
+    }
+
+    return Preview()
 }
